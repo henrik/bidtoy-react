091871e1c3dc542b4be9400015bfe5b0610d17fb
Henrik Nyh <henrik@nyh.se>
2014-12-28 21:48:36 +0100

    More CS

--- coffee.coffee
+++ coffee.coffee
@@ -1,3 +1,6 @@
+COLORS = [ "_", "powderblue", "chartreuse", "yellow", "pink", "#eee" ]
+BID_STEP = 50
+
 window.PubSub =
   subs: {}
   sub: (name, cb) ->
@@ -30,3 +33,53 @@ window.BidRow = React.createClass
       (td {}, @props.bid.time)
       (td className: @reserveClasses(), formatNumber(@props.bid.amount))
     )
+
+window.BidForm = React.createClass
+  componentDidMount: ->
+    # Trying out using pubsub to sync the two components.
+    # Another alternative to look into: an owner component passing its state into owned component props.
+    that = this
+    PubSub.sub "fetchedBids", (bids) ->
+      that.setState leadingAmount: bids[0].amount
+
+  nextAmount: ->
+    @state.leadingAmount + BID_STEP
+
+  handleSubmit: (e) ->
+    e.preventDefault()
+    amountField = @refs.amount.getDOMNode()
+    buyerField = @refs.buyer.getDOMNode()
+    amount = amountField.value || @nextAmount()
+    buyer = buyerField.value
+
+    postData =
+      amount: amount
+      buyer: buyer
+
+    $.post("/bid.json", postData).success (data) ->
+      amountField.value = ""
+      PubSub.pub("bidPlaced", data)
+
+  render: ->
+    # Don't render until we know the amount.
+    return React.DOM.div({}, "Loading…") unless @state
+
+    buyerOpts = [ 1, 2, 3, 4, 5 ].map (n) -> React.DOM.option(key: n, n)
+
+    { div, p, form, select, input, button } = React.DOM
+
+    (div {},
+      (p {}, "Leading bid: #{formatNumber @state.leadingAmount} SEK")
+      (form onSubmit: @handleSubmit,
+        (p {},
+          "Bid as buyer #"
+          (select ref: "buyer", buyerOpts)
+        )
+        (p {},
+          (input type: "number", ref: "amount", placeholder: formatNumber(@nextAmount()), min: @nextAmount(), step: BID_STEP)
+          (button {}, "Place bid")
+        )
+      )
+    )
+
+React.render React.createElement(BidForm), document.getElementById("bid-form")
--- public/app.js
+++ public/app.js
@@ -1,63 +1,6 @@
-const COLORS = [ "_", "powderblue", "chartreuse", "yellow", "pink", "#eee" ];
 const TRUNCATE_AT = 3;
-const BID_STEP = 50;
 
 var ReactCSSTransitionGroup = React.addons.CSSTransitionGroup;
-
-var BidForm = React.createClass({
-  componentDidMount: function() {
-    // Trying out using pubsub to sync the two components.
-    // Another alternative to look into: an owner component passing its state into owned component props.
-    var that = this;
-    PubSub.sub("fetchedBids", function(bids) {
-      var leadingAmount = bids[0].amount;
-      that.setState({ leadingAmount: leadingAmount });
-    });
-  },
-
-  nextAmount: function() {
-    return this.state.leadingAmount + BID_STEP;
-  },
-
-  handleSubmit: function(e) {
-    e.preventDefault();
-    var amountField = this.refs.amount.getDOMNode();
-    var buyerField = this.refs.buyer.getDOMNode();
-    var amount = amountField.value || this.nextAmount();
-    var buyer = buyerField.value;
-
-    $.post("/bid.json", {
-      amount: amount,
-      buyer: buyer
-    }).success(function(data) {
-      amountField.value = "";
-      PubSub.pub("bidPlaced", data);
-    });
-  },
-
-  render: function() {
-    // Don't render until we know the amount.
-    if (!this.state) return <div>Loading…</div>;
-
-    var buyerOpts = [1, 2, 3, 4, 5].map(function(n) { return <option key={n}>{n}</option>; });
-
-    return <div>
-      <p>Leading bid: {formatNumber(this.state.leadingAmount)} SEK</p>
-      <form onSubmit={this.handleSubmit}>
-        <p>
-          Bid as buyer #
-          <select ref="buyer">{buyerOpts}</select>
-        </p>
-        <p>
-          <input type="number" ref="amount" placeholder={formatNumber(this.nextAmount())} min={this.nextAmount()} step={BID_STEP} />
-          &nbsp;
-          <button>Place bid</button>
-        </p>
-      </form>
-    </div>;
-  }
-});
-
 var BidTable = React.createClass({
   componentDidMount: function() {
     this.fetchBids();
@@ -119,11 +62,6 @@ var BidTable = React.createClass({
     </div>;
   }
 });
-
-React.render(
-  <BidForm />,
-  document.getElementById("bid-form")
-);
 
 React.render(
   <BidTable />,
